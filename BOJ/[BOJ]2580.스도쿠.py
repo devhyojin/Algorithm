4dc7c27d3bb365049718bc5ae877004195d1b43b
devhyojin <dev.hyojinkim@gmail.com>
2021-07-18 23:47:21 +0900

    [BOJ] edit 2580

--- BOJ/[BOJ]2580.스도쿠.py
+++ BOJ/[BOJ]2580.스도쿠.py
@@ -1,43 +1,35 @@
-from sys import stdin
+import sys
+
+def find_candidates(r, c):
+    candidates = set(range(1,10))
+    #해당 행에 존재하는 것은 후보군에서 제외한다.
+    candidates -= set(board[r])
+    #해당 열에 존재하는 것은 후보군에서 제외한다.
+    col_nums = set()
+    for i in range(9):
+        col_nums.add(board[i][c])
+    candidates -= col_nums
+    #해당 사각형에 존재하는 것은 후보군에서 제외한다.
+    sqr_nums = set()
+    sqr_r, sqr_c = r//3*3, c//3*3
+    for i in range(sqr_r,sqr_r+3):
+        for j in range(sqr_c, sqr_c+3):
+            sqr_nums.add(board[i][j])
+    candidates -= sqr_nums
+    return tuple(candidates)
+
 def fill_sudoku(b):
     if b == len(blanks):
-        for row in board:
-            print(' '.join(row))
-        return
-    bi, bj = blanks[b]
-    di, dj = bi-(bi%3), bj-(bj%3)
-
-    for i in range(9):
-        row_check(i, bi)
-        col_check(i, bj)
-    sqr_check(di,dj)
-
-    can_num = []
-    for i in range(10):
-        if not onenine[i]:
-            can_num.append(i)
-    for num in can_num:
-        board[bi][bj] = num
+        [print(*row) for row in board]
+        sys.exit()
+    br, bc = blanks[b]
+    candidates = find_candidates(br, bc)
+    for can in candidates:
+        board[br][bc] = can
         fill_sudoku(b+1)
-        board[bi][bj] = 0
-def row_check(x, bx):
-    room = board[bx][x]
-    if room:
-        onenine[room] = True
-    return
-def col_check(x, bx):
-    room = board[x][bx]
-    if room:
-        onenine[room] = True
-    return
-def sqr_check(by, bx):
-    for i in range(by, by+3):
-        for j in range(bx, bx+3):
-            room = board[i][j]
-            if room:
-                onenine[room] = True
-    return
-board = [list(map(int, stdin.readline().split())) for _ in range(9)]
+        board[br][bc] = 0
+
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(9)]
 blanks=[(i,j) for i in range(9) for j in range(9) if board[i][j]==0]
-onenine = [True] + [False]*9
+
 fill_sudoku(0)
